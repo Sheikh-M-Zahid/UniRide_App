f2f0a4ccdef85474c44d3f99c90a6814de7ea81d
MD Zahid Hossain <mdzahedhasann@gmail.com>
2026-04-14 07:37:30 +0600

    Add authentication middleware to help request route

--- routes/helpRoutes.js
+++ routes/helpRoutes.js
@@ -2,8 +2,12 @@ const express = require('express');
 const router = express.Router();
 
 const helpController = require('../controllers/helpController');
+const authMiddleware = require('../middlewares/authMiddleware');
 
 // PUBLIC
 router.get('/faqs', helpController.getFaqs);
+
+// PRIVATE
+router.post('/submit', authMiddleware, helpController.submitHelpRequest);
 
 module.exports = router;
