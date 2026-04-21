14ae25cd906d36f404b2e6c368e78375dc0c44f1
MD Zahid Hossain <mdzahedhasann@gmail.com>
2026-04-22 05:57:26 +0600

    Refactor companySharingRoutes to use controller alias

--- routes/companySharingRoutes.js
+++ routes/companySharingRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const companySharingController = require('../controllers/companySharingController');
+const controller = require('../controllers/companySharingController');
 const authMiddleware = require('../middlewares/authMiddleware');
 const { validateRequiredFields } = require('../middlewares/validateMiddleware');
 
@@ -8,19 +8,24 @@ router.post(
   '/',
   authMiddleware,
   validateRequiredFields(['start_location', 'destination']),
-  companySharingController.createSession
+  controller.createSession
 );
 
-router.post('/:sessionId/join', authMiddleware, companySharingController.joinSession);
-router.get('/', authMiddleware, companySharingController.listSessions);
+router.get('/my-active', authMiddleware, controller.getMyActiveSession);
+router.get('/:sessionId', authMiddleware, controller.getSessionById);
+router.post('/:sessionId/join', authMiddleware, controller.joinSession);
+router.patch('/:sessionId/cancel', authMiddleware, controller.cancelSession);
+router.patch('/:sessionId/start', authMiddleware, controller.startSession);
+router.post('/:sessionId/location', authMiddleware, controller.updateLiveLocation);
+router.get('/:sessionId/location', authMiddleware, controller.getLiveLocation);
+router.get('/', authMiddleware, controller.listSessions);
 
 router.post(
   '/:sessionId/chat',
   authMiddleware,
   validateRequiredFields(['message_text']),
-  companySharingController.sendCompanyChatMessage
+  controller.sendCompanyChatMessage
 );
-
-router.get('/:sessionId/chat', authMiddleware, companySharingController.fetchCompanyChatMessages);
+router.get('/:sessionId/chat', authMiddleware, controller.fetchCompanyChatMessages);
 
 module.exports = router;
