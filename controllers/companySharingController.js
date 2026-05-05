cf6d7f3d45492b211c9e8e64ca07ed91d622fcba
MD Zahid Hossain <mdzahedhasann@gmail.com>
2026-05-05 12:00:38 +0600

    Add removeParticipant and getSessionWithParticipants methods

--- controllers/companySharingController.js
+++ controllers/companySharingController.js
@@ -62,6 +62,23 @@ const fetchCompanyChatMessages = asyncHandler(async (req, res) => {
   return successResponse(res, 'Messages fetched.', data);
 });
 
+const removeParticipant = asyncHandler(async (req, res) => {
+  const data = await service.removeParticipant(
+    req.params.sessionId,
+    req.user.userId,
+    req.params.participantUserId
+  );
+  return successResponse(res, 'Participant removed.', data);
+});
+
+const getSessionWithParticipants = asyncHandler(async (req, res) => {
+  const data = await service.getSessionWithParticipants(
+    req.params.sessionId,
+    req.user.userId
+  );
+  return successResponse(res, 'Session details fetched.', data);
+});
+
 module.exports = {
   createSession,
   getSessionById,
@@ -74,4 +91,6 @@ module.exports = {
   listSessions,
   sendCompanyChatMessage,
   fetchCompanyChatMessages,
+  removeParticipant,
+  getSessionWithParticipants,
 };
