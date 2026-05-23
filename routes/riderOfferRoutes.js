7f8751f81b27306b982809d0df77e784c6000667
MD Zahid Hossain <mdzahedhasann@gmail.com>
2026-05-23 22:40:41 +0600

    Refactor authentication middleware import and usage

--- routes/riderOfferRoutes.js
+++ routes/riderOfferRoutes.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { authenticate } = require('../middlewares/authMiddleware');
+const authMiddleware = require('../middlewares/authMiddleware');
 const riderOfferController = require('../controllers/riderOfferControllers');
 
 // GET /api/rider/offers
-router.get('/', authenticate, riderOfferController.getRiderOffers);
+router.get('/', authMiddleware, riderOfferController.getRiderOffers);
 
 module.exports = router;
