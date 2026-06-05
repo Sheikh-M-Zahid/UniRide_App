ae0b1761212a00dfbb31b3c7a02cc5ef00124b9a
MD Zahid Hossain <mdzahedhasann@gmail.com>
2026-06-06 01:17:04 +0600

    Update Firebase initialization with env variables
    Refactor Firebase initialization to use environment variables for credentials.

--- config/firebase.js
+++ config/firebase.js
@@ -1,10 +1,25 @@
 const admin = require('firebase-admin');
-const serviceAccount = require('../firebase-service-account.json');
 
 if (!admin.apps.length) {
-  admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-  });
+  const privateKey = process.env.FIREBASE_PRIVATE_KEY
+    ? process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n')
+    : undefined;
+
+  if (
+    process.env.FIREBASE_PROJECT_ID &&
+    process.env.FIREBASE_CLIENT_EMAIL &&
+    privateKey
+  ) {
+    admin.initializeApp({
+      credential: admin.credential.cert({
+        projectId: process.env.FIREBASE_PROJECT_ID,
+        clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+        privateKey: privateKey,
+      }),
+    });
+  } else {
+    console.warn('Firebase env variables missing. Push notifications disabled.');
+  }
 }
 
 module.exports = admin;
