d42bd6cb6cba1cf3b78354fa31a5b687c6d31232
MD Zahid Hossain <mdzahedhasann@gmail.com>
2026-06-09 16:49:12 +0600

    Add route to complete ride from map

--- routes/riderMapRoutes.js
+++ routes/riderMapRoutes.js
@@ -11,5 +11,6 @@ router.get('/route-polyline', riderMapController.getRoutePolyline);
 router.post('/location', riderMapController.updateLocation);
 router.post('/ride-requests/:requestId/accept', riderMapController.acceptRequest);
 router.post('/ride/:rideId/start-navigation', riderMapController.startNavigation);
+router.post('/ride/:rideId/complete', riderMapController.completeRideFromMap);
 
 module.exports = router;
