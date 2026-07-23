0835f42636ffaabe40155c4af3347159c2a8a44d
Md. Zahid Hossain <2024-1-60-074@std.ewubd.edu>
2026-07-23 09:57:44 +0600

    Rename authentication middleware import

--- routes/alumni.routes.js
+++ routes/alumni.routes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const authenticate = require('../middleware/authenticate');
+const authenticate = require('../middlewares/authMiddleware');
 const alumniController = require('../controllers/alumni.controller');
 const { alumniUpload } = require('../utils/upload');
 
